b04c35edf27e7202c6275b5ec7e52d2b59983b33
Chris Turner <mr.chris.turner@gmail.com>
2016-09-21 17:49:58 -0700

    first commit, finished a couple setup items and some reqs x2

---
+++ app/models/place.rb
@@ -0,0 +1,18 @@
+class Place
+
+def self.mongo_client
+	@@db = Mongo::Client.new('mongodb://localhost:27017/test')
+end
+
+def self.collection
+  self.mongo_client['racers']
+end
+
+def self.load_all(file)
+	docs = JSON.parse(file.read)
+	collection.insert_many(docs)
+end
+
+
+
+end
---
+++ app/models/point.rb
@@ -0,0 +1,22 @@
+class Point
+	attr_accessor :longitude, :latitude
+
+def intialize(params)
+	if !params[:coordinates].nil?
+		@longitude = params[:coordinates][0]
+		@latitude = params[:coordinates][1]
+	else
+		@longitude = params[:lng]
+		@latitude = params[:lat]
+	end
+end
+
+def to_hash
+	{
+		:type => "Point",
+		:coordinates => [@latitude, @longitude]
+	}
+end
+
+
+end
