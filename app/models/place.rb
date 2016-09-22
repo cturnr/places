7e08fbe33a54d3a4efbd92b870ee20ae5cb82d41
Chris Turner <mr.chris.turner@gmail.com>
2016-09-21 23:06:56 -0700

    trying to get more done, think there is an issue with my place initialize ...

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place
+	attr_accessor :id, :formatted_address, :location, :address_components
 
 	def self.mongo_client
 		@@db = Mongo::Client.new('mongodb://localhost:27017/test')
@@ -13,6 +14,21 @@ class Place
 		collection.insert_many(docs)
 	end
 
+  def initialize(params={})
+    @id = params[:_id].to_s
+    @address_components = params[:address_components].map{ |a| AddressComponent.new(a)} if !params[:address_components].nil?
+    @formatted_address = params[:formatted_address]
+    # @location = Point.new(params[:geometry][:geolocation])
+  end
 
+   def self.find_by_short_name(short_name)
+    collection.find(:'address_components.short_name' => short_name)
+  end
+
+  def self.to_places(places)
+    places.map do |place|
+      return(Place.new(place))
+    end
+  end
 
 end
