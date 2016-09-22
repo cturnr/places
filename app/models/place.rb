a7fdbaa0c959e1a8b8a1a95b53a18a3d22f581a6
Chris Turner <mr.chris.turner@gmail.com>
2016-09-21 21:29:12 -0700

    Completed the Address Component Class

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,17 +1,17 @@
 class Place
 
-def self.mongo_client
-	@@db = Mongo::Client.new('mongodb://localhost:27017/test')
-end
+	def self.mongo_client
+		@@db = Mongo::Client.new('mongodb://localhost:27017/test')
+	end
 
-def self.collection
-  self.mongo_client['racers']
-end
+	def self.collection
+	  self.mongo_client['racers']
+	end
 
-def self.load_all(file)
-	docs = JSON.parse(file.read)
-	collection.insert_many(docs)
-end
+	def self.load_all(file)
+		docs = JSON.parse(file.read)
+		collection.insert_many(docs)
+	end
 
 
 
